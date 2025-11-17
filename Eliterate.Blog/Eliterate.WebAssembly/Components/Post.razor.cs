abd8547db2251f4177cd83060774fb2c9c952300
Eli <elias.styner@gmail.com>
2025-11-17 01:03:13 +0100

    some small work for getting this blog running

---
+++ Eliterate.Blog/Eliterate.WebAssembly/Components/Post.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace Eliterate.WebAssembly.Components
+{
+    public partial class Post(string title, string text)
+    {
+        public string Title { get; set; } = title;
+        public string Text { get; set; } = text;
+    }
+}
---
+++ Eliterate.Blog/Eliterate.WebAssembly/Models/BlogPost.cs
@@ -0,0 +1,9 @@
+﻿namespace Eliterate.WebAssembly;
+
+public class BlogPost
+{
+    public string Title { get; set; } = "";
+    public string[] Tags { get; set; } = [];
+    public DateTime Posted { get; set; }
+    public string Text { get; set; } = "";
+}
