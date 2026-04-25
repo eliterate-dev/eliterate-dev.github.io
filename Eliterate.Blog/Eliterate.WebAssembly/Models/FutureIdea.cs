1e0a19e1d2921d1d5064702a2dfb947dca91e646
Eli <elias.styner@gmail.com>
2026-04-25 21:15:03 +0200

    added tearoffs to ideas

--- Eliterate.Blog/Eliterate.WebAssembly/Models/FutureIdea.cs
+++ Eliterate.Blog/Eliterate.WebAssembly/Models/FutureIdea.cs
@@ -4,4 +4,6 @@ public class FutureIdea
 {
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string? LinkText { get; set; }
+    public string? Link { get; set; }
 }
