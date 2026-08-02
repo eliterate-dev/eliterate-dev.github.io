4dea153fcec3fc5685d82d9af9306f6b2b8f11a8
Elias-Styner <elias.styner@gmail.com>
2026-08-02 12:10:57 +0200

    fixed unfinshed blogpost being shown in latest

--- Eliterate.Blog/Eliterate.WebAssembly/Services/ContentService.cs
+++ Eliterate.Blog/Eliterate.WebAssembly/Services/ContentService.cs
@@ -25,14 +25,7 @@ public class ContentService(HttpClient client) : IContentService
         var metadata = await _client.GetFromJsonAsync<IEnumerable<PostMetadata>>($"posts/metadata.json");
         if (metadata is null)
             return [];
-        var posts = new List<PostMetadata>();
-        foreach (var item in metadata)
-        {
-            if (item is null || !item.IsActive || !item.ShowInList)
-                continue;
-            posts.Add(item);
-        }
-        return [.. posts.OrderByDescending(p => p.Edited ?? p.Created)];
+        return [.. metadata.Where(p => p.IsActive && p.ShowInList).OrderByDescending(p => p.Edited ?? p.Created)];
     }
 
     public async Task<IEnumerable<PostMetadata>> GetMetadataByTag(string tagId)
@@ -40,14 +33,7 @@ public class ContentService(HttpClient client) : IContentService
         var metadata = await _client.GetFromJsonAsync<IEnumerable<PostMetadata>>($"posts/metadata.json");
         if (metadata is null)
             return [];
-        var posts = new List<PostMetadata>();
-        foreach (var item in metadata)
-        {
-            if (item is null || !item.IsActive || !item.Tags.Any(t => t.ToUrlSafeString() == tagId))
-                continue;
-            posts.Add(item);
-        }
-        return [.. posts.OrderByDescending(p => p.Edited ?? p.Created)];
+        return [.. metadata.Where(p => p.IsActive && p.ShowInList && !p.Tags.Any(t => t.ToUrlSafeString() == tagId)).OrderByDescending(p => p.Edited ?? p.Created)];
     }
 
     public async Task<BlogPost?> GetPost(string title)
@@ -64,7 +50,7 @@ public class ContentService(HttpClient client) : IContentService
     public async Task<BlogPost?> GetLatest()
     {
         var metadata = await _client.GetFromJsonAsync<IEnumerable<PostMetadata>>($"posts/metadata.json");
-        var post = metadata?.OrderByDescending(p => p.Created).FirstOrDefault();
+        var post = metadata?.Where(p => p.IsActive).OrderByDescending(p => p.Created).FirstOrDefault();
         if (post is null)
             return null;
         var markdown = await _client.GetStringAsync(post.ContentUrl);
